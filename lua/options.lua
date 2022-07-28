78adbf0fb3442c95916fb25faa815813eb647ac6
robhhr <robhhr@outlook.com>
2022-07-28 10:08:22 -0700

    add more options

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,15 @@
 local options = {
 	background = 'dark',
+	cursorline = true,
+	fileencoding = 'utf-8',
 	hidden = true,
 	number = true,
 	relativenumber = true,
 	showmatch = true,
+	swapfile = false,
+	tabstop = 2,
+	termguicolors = true
+	whichwrap = 'b,s,<,>,[,],h,l'
 }
 
 for i, j in pairs(options) do
