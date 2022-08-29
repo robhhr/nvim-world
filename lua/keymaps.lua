8574336197f00d9db9986dc7123234a3683e6c52
robhhr <robhhr@outlook.com>
2022-08-29 09:50:19 -0700

    fix indent and add init keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,21 @@
+local opts = {noremap=true, silent = true}
+local term_opts = {silent = true}
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- save
+keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>wq", ":wq<CR>", opts)
+keymap("n", "<leader>q", ":q!<CR>", opts)
+
+keymap("n", "<leader><tab>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader><tab>", ":NvimTreeToggle<CR>", opts)
+
+keymap("n", "<leader><Right>", ":BufferLineCycleNext<CR>", opts)
+keymap("n", "<leader><Left>", ":BufferLineCyclePrev<CR>", opts)
+
+keymap("n", "<leader>ww", ":Bwipeout<CR>", opts)
+keymap("n", "<leader>qq", ":Bwipeout!<CR>", opts)
