d83020baf9a8dbd3022ccf60c20f8d9701be8f2c
robhhr <robhhr@outlook.com>
2022-10-24 11:24:45 -0700

    clean up config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,26 +1,12 @@
-local opts = {noremap=true, silent = true}
-local term_opts = {silent = true}
+local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+keymap("", ",", "<Nop>", opts)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
--- save
-keymap("n", "<leader>w", ":w<CR>", opts)
-keymap("n", "<leader>wq", ":wq<CR>", opts)
+-- exit w/o save
 keymap("n", "<leader>q", ":q!<CR>", opts)
 
-keymap("n", "<leader><tab>", ":NvimTreeToggle<CR>", opts)
-keymap("n", "<leader><tab>", ":NvimTreeToggle<CR>", opts)
-
-keymap("n", "<leader><Right>", ":BufferLineCycleNext<CR>", opts)
-keymap("n", "<leader><Left>", ":BufferLineCyclePrev<CR>", opts)
-
-keymap("n", "<leader>ww", ":Bwipeout<CR>", opts)
-keymap("n", "<leader>qq", ":Bwipeout!<CR>", opts)
-
-keymap("n", "<leader>p", ":Telescope find_files<CR>", opts)
-
-keymap("n", "<leader>ll", ":luafile ~/.config/nvim/lua/init.lua<CR>", opts)
-
+-- reload config file
+keymap("n", "<leader>sv", ":luafile ~/.config/nvim/lua/init.lua<CR>", opts)
