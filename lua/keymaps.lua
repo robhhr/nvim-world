b9e10472ce9330499b89e9562f3eeaec3d8511ce
robhhr <robhhr@outlook.com>
2023-03-16 23:24:59 -0700

    modif key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,8 +2,8 @@ local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
 keymap("", ",", "<Nop>", opts)
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- exit
 keymap("n", "<leader>q", ":q<CR>", opts)
