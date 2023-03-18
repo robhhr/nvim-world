21cb4afbc302c97a84c662032d1631fbd4e83e82
robhhr <robhhr@outlook.com>
2023-03-18 16:13:29 -0700

    add gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,5 +1,5 @@
 require('gitsigns').setup({
   numhl = true,
   current_line_blame = true,
-  current_line_blame_formatter = '<author> · <author_time:%d/%b/%y @ %H:%M> · <summary>'
+  current_line_blame_formatter = '<committer> · <author_time:%d/%b/%y @ %H:%M> ▶ <summary>'
 })
