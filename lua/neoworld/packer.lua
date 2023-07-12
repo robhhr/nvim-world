b2cbe8ce0f241907eff2c4632f201b0760767818
robhhr <robhhr@outlook.com>
2023-07-12 13:55:24 -0700

    re-add telescope

--- lua/neoworld/packer.lua
+++ lua/neoworld/packer.lua
@@ -18,10 +18,10 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- use {
-  --   'nvim-telescope/telescope.nvim', tag = '0.1.1',
-  --   requires = { { 'nvim-lua/plenary.nvim' } }
-  -- }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
   --[[ start of themes ]]
   -- use({
