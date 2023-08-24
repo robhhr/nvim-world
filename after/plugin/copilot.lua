90cacc6efe72ddb18c237023d968c4bd1cc7961f
robhhr <robhhr@outlook.com>
2023-08-24 11:51:28 -0700

    update copilot binds

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -7,10 +7,14 @@ global.copilot_filetypes = { xml = false }
 -- for disabling all and enabling specific filetypes
 -- vim.g.copilot_filetypes = {["*"] = false, python = true }
 
+-- remap tab for enter
+global.copilot_no_tab_map = true
+keymap.set("i", "<M-Space>", "copilot#Accept('\\<CR>')<CR>", { expr = true, silent = true })
+
 -- remap (cycle to next suggestion)
-keymap.set("i", "<C-Down>", "copilot#Next()", { expr = true, silent = true })
+keymap.set("i", "<M-Down>", "copilot#Next()", { expr = true, silent = true })
 
 -- remap (cycle to prev suggestion)
-keymap.set("i", "<C-Up>", "copilot#Previous()", { expr = true, silent = true })
+keymap.set("i", "<M-Up>", "copilot#Previous()", { expr = true, silent = true })
 
 vim.cmd [[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
