572e016df9a6c0b05c21e557425e8a2a87ec2d48
robhhr <robberto@hey.com>
2023-10-15 05:19:23 -0700

    readd copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -2,14 +2,14 @@ local keymap = vim.keymap
 local global = vim.g
 
 -- for basic disable/enable filetypes
-global.copilot_filetypes = { xml = false }
+global.copilot_filetypes = { ["*"] = true, xml = false }
 
 -- for disabling all and enabling specific filetypes
 -- vim.g.copilot_filetypes = {["*"] = false, python = true }
 
 -- remap tab for enter
 global.copilot_no_tab_map = true
--- keymap.set("i", "<M-Right>", "copilot#Accept('<CR>')", { expr = true, silent = true })
+keymap.set("i", "<M-Right>", "copilot#Accept('<CR>')", { expr = true, silent = true })
 
 -- remap (cycle to next suggestion)
 keymap.set("i", "<M-Down>", "copilot#Next()", { expr = true, silent = true })
