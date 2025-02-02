2f0173b8d97393260cc8bca78655d4f4518ad07e
robhhr <robertohhr@pm.me>
2025-02-01 21:47:13 -0800

    disable blank line on save

--- lua/neoworld/remap.lua
+++ lua/neoworld/remap.lua
@@ -52,15 +52,3 @@ keymap.set('n', '<s-tab>',
 keymap.set("n", "<leader>h", ":HopWord<CR>")
 
 keymap.set("n", "<leader>g", ":LazyGit<CR>")
-
-vim.api.nvim_create_autocmd({ "BufWritePre" },
-  {
-    group = vim.api.nvim_create_augroup('UserOnSave', {}),
-    pattern = '*',
-    callback = function()
-      local n_lines = vim.api.nvim_buf_line_count(0)
-      local last_nonblank = vim.fn.prevnonblank(n_lines)
-      if last_nonblank <= n_lines then vim.api.nvim_buf_set_lines(0, last_nonblank, n_lines, true, { '' }) end
-    end,
-  }
-)
