283a39a6e2537b7b9d619eb77bf664c1ad7ec369
robhhr <robertohhr@pm.me>
2025-04-01 23:45:45 -0700

    update dashboard

--- lua/neoworld/plugins/dashboard-nvim.lua
+++ lua/neoworld/plugins/dashboard-nvim.lua
@@ -13,34 +13,42 @@ __________________        ____________________________________________
 -- logo = string.rep("\n", 8) .. logo .. "\n\n"
 
 db.setup({
-    theme = 'hyper',
-    config = {
-        header = vim.split(logo, "\n"),
-        -- week_header = {
-        --     enable = true,
-        -- },
-        shortcut = {
-            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-            {
-                icon = ' ',
-                icon_hl = '@variable',
-                desc = 'Files',
-                group = 'Label',
-                action = 'Telescope find_files',
-                key = 'f',
-            },
-            {
-                desc = ' Apps',
-                group = 'DiagnosticHint',
-                action = 'Telescope app',
-                key = 'a',
-            },
-            {
-                desc = ' dotfiles',
-                group = 'Number',
-                action = 'Telescope dotfiles',
-                key = 'd',
-            },
-        },
+  theme = 'hyper',
+  config = {
+    header = vim.split(logo, "\n"),
+    week_header = {
+      enable = true,
     },
+    shortcut = {
+      { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+      {
+        icon = ' ',
+        icon_hl = '@variable',
+        desc = 'Files',
+        group = 'Label',
+        action = 'Telescope find_files',
+        key = 'f',
+      },
+      {
+        desc = ' Apps',
+        group = 'DiagnosticHint',
+        action = 'Telescope app',
+        key = 'a',
+      },
+      {
+        desc = ' dotfiles',
+        group = 'Number',
+        action = 'Telescope dotfiles',
+        key = 'd',
+      },
+    },
+    project = {
+      limit = 7,
+      -- label = '',
+      action = 'Telescope find_files cwd='
+    },
+    mru = {
+      limit = 7
+    }
+  },
 })
