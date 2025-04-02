5c9681362e39d393a5d2208a305b06e5d2665aef
robhhr <robertohhr@pm.me>
2025-04-01 22:46:04 -0700

    replace md viewer

--- lua/neoworld/config.lua
+++ lua/neoworld/config.lua
@@ -162,10 +162,11 @@ require("lazy").setup({
   },
 
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
 
   {
