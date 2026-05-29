89c30f6741a74025fb8f7a05de9934dc93b9865f
robhhr <robertohhr@pm.me>
2026-05-29 01:07:54 -0700

    fix: go treesitter for gomod & gosum

--- lua/neoworld/plugins/treesitter.lua
+++ lua/neoworld/plugins/treesitter.lua
@@ -1,4 +1,11 @@
 require('nvim-treesitter').setup({
-  ensure_installed = { "javascript", "json", "lua", "php", "rust", "scss", "typescript", "vim", "go" },
+  ensure_installed = { "javascript", "json", "lua", "php", "rust", "scss", "typescript", "vim", "go", "gomod", "gosum" },
   auto_install = true,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'gomod', 'gosum' },
+  callback = function()
+    vim.treesitter.start()
+  end,
 })
