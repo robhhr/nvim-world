1384f80b2cf16000499dd822d177d4235f1fb48f
robhhr <robertohhr@pm.me>
2026-07-10 23:49:18 -0700

    chore: update harpoon to v2

--- lua/neoworld/plugins/harpoon.lua
+++ lua/neoworld/plugins/harpoon.lua
@@ -1,8 +1,3 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<C-j>", function() ui.nav_prev() end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_next() end)
+harpoon:setup()
